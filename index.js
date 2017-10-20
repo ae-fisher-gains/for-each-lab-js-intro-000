47f68e6b08382c0551791803d230e5a67b89efbf
Adam Fisher <ae.fisher.gains@gmail.com>
2017-10-20 14:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function log(element, index, array) {
-  console.log(`${index}: ${element}`)
-}
-
 function iterativeLog(array) {
-  array.forEach(function log(element, index, array))
+  array.forEach(function log(element, index, array) {
+    console.log(`${index}: ${element}`
+  }
+)
 }
